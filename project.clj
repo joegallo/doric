45a357adf292df53da01b2c7996ff1b9d1663d99
Joe Gallo <joegallo@gmail.com>
2011-02-28 09:14:22 -0500

    clean up project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject doric "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject doric "0.5.0-SNAPSHOT"
+  :description "Clojure table layout"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
