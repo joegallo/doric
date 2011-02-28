b07b9c414eb82e7d1f65a9720390a150802b4f2a
Joe Gallo <joegallo@gmail.com>
2011-02-28 12:10:54 -0500

    release 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject doric "0.5.0-SNAPSHOT"
+(defproject doric "0.5.0"
   :description "Clojure table layout"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
