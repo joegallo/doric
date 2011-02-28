1e250dbd03fc558ad1df4c1c9fba355c0e72fbf7
Joe Gallo <joegallo@gmail.com>
2011-02-28 12:15:19 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject doric "0.5.0"
+(defproject doric "0.6.0-SNAPSHOT"
   :description "Clojure table layout"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
