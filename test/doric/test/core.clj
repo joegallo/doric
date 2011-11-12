d65790e6e831de89986d24dfa0bc4056fb34123b
Joe Gallo <joegallo@gmail.com>
2011-11-12 16:33:25 -0500

    add a test for laziness

--- test/doric/test/core.clj
+++ test/doric/test/core.clj
@@ -83,3 +83,25 @@
     (is (.contains rendered "| 1 | 2 |"))
     (is (.contains rendered "| 3 | 4 |"))
     (is (.contains rendered "|---+---|"))))
+
+(deftest test-table*-laziness
+  (let [calls (atom 0)
+        inc #(do (swap! calls inc) %)]
+    (testing "formats are not lazy"
+      (let [seq (table* [{:name :1 :format inc}
+                         {:name :2 :format inc}]
+                        [{:1 3 :2 4}])]
+        (is (= 2 @calls))))
+    (reset! calls 0)
+    (testing "unless you provide widths"
+      (let [seq (table* [{:name :1 :format inc :width 10}
+                         {:name :2 :format inc :width 10}]
+                        [{:1 3 :2 4}])]
+        (is (= 0 @calls))))
+    (reset! calls 0)
+    (testing "even for formats that should be automatically lazy, like csv"
+      (let [seq (table* ^{:format csv}
+                        [{:name :1 :format inc :width 0}
+                         {:name :2 :format inc :width 0}]
+                        [{:1 3 :2 4}])]
+        (is (= 0 @calls))))))
