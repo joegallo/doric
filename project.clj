5dace7cc9217a830ab8dc84b2661ee70f6ae36e8
Joe Gallo <joegallo@gmail.com>
2013-01-13 20:03:27 -0500

    clojure and leiningen updates

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject doric "0.8.0-SNAPSHOT"
   :description "Clojure table layout"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[org.apache.poi/poi "3.7"]]
-  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]]})
+  :url "https://github.com/joegallo/doric"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :dev {:dependencies [[org.apache.poi/poi "3.7"]]}})
