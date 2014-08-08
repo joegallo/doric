8c8c103adaf9d0036666ff0271dfa886f905409e
Joe Gallo <joegallo@gmail.com>
2014-08-08 17:32:50 -0400

    no need to include this anymore

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/joegallo/doric"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
